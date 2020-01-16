1c50b77c7a38e58ac58678aadbe7d085488baaee
rex2630 <rex2630@gmail.com>
2020-01-16 17:37:35 +0100

    Replace __resource.lua with fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'ui/index.html'
+
+files {
+  'ui/index.html',
+  'ui/style.css',
+  'ui/img/bg-img.png',
+  'ui/script.js'
+}
